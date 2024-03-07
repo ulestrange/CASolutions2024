d12d926aa111e21963e8edbb35eab4864ef761ee
Una L <una.lestrange@atu.ie>
2024-03-07 13:56:01 +0000

    Add project files.

---
+++ CA2/Program.cs
@@ -0,0 +1,206 @@
+﻿using System.ComponentModel;
+
+namespace CA2
+{
+    internal class Program
+    {
+
+        static string[] bookIDs = new string[5];
+        static double[] scores = new double[5];
+        static int count = 0;
+
+        static string bestBookID = "";
+        static double bestScore = -1;
+
+        // static string[] categoryDescriptions = { "0 -1", "1+ -2", "2+ -3", "3+ -4", "4+ -5" };
+        static string[] categoryDescriptions = { "Poor", "Fair", "Good", "Very Good", "Excellant" };
+
+        static int[] categoryCounts = new int[5];
+        static double[] categorySums = new double[5];
+        static void Main(string[] args)
+        {
+
+
+            while (count < 5)
+            {
+
+                string bookID = GetBookIDFromUser();
+
+                if (bookID == "-999")
+                {
+                    break;
+                }
+                bookIDs[count] = bookID;
+
+                
+
+                count++;
+
+                double score = GetScoreFromUser();
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestBookID = bookID;
+                }
+                scores[count] = score;
+
+                string categoryDescription = GetCategoryDescription(scores[count]);
+
+                Console.WriteLine($"Book {bookIDs[count]} is rated as {categoryDescription}");
+
+                int categoryIndex = Array.IndexOf(categoryDescriptions, categoryDescription);
+
+                categoryCounts[categoryIndex]++;
+                categorySums[categoryIndex] += scores[count];
+            }
+
+            // here we have finished taking input and need to
+            // print the book report and rating report
+
+            Console.WriteLine("-----------------------------");
+            PrintBookReport();
+            Console.WriteLine("-----------------------------");
+
+            PrintRatingReport();
+
+
+
+        }
+
+        static void PrintBookReport()
+        {
+            Console.WriteLine("Book Report");
+            Console.WriteLine("Book ID    Score");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{bookIDs[i]}    {scores[i]}");
+            }
+        }
+
+        static void PrintRatingReport()
+        {
+            Console.WriteLine("Rating Report");
+            Console.WriteLine("Rating    Number of Books   Average Score");
+            int countReviews = 0;
+            double sumRevies = 0;
+            for (int i = 0; i < categoryDescriptions.Length; i++)
+            {
+
+                countReviews += categoryCounts[i];
+                sumRevies += categorySums[i];
+                if (categoryCounts[i] == 0)
+                {
+                    Console.WriteLine($"{categoryDescriptions[i],-10}  {categoryCounts[i],-17}  ");
+                }
+                else
+                {
+                    Console.WriteLine($"{categoryDescriptions[i],-10} " +
+                        $" {categoryCounts[i],-17} {categorySums[i] / categoryCounts[i]} ");
+                }
+            }
+
+            Console.WriteLine($"Total Reviews : {countReviews}");
+
+            if (countReviews != 0)
+            {
+                Console.WriteLine($"Overall Average {sumRevies/countReviews}");
+                Console.WriteLine($"Book with Highest Score : {bestBookID}");
+            }
+
+        }
+
+        //
+
+        static string GetBookIDFromUser()
+        {
+            Console.Write("Please enter a Book ID (or -999 to finish) > ");
+            string bookID = Console.ReadLine().ToUpper();
+            while (!IsValidBookID(bookID) && bookID != "-999")
+            {
+                Console.WriteLine("Invalid bookID - please enter a Book ID (or -999 to finish) >");
+                bookID = Console.ReadLine().ToUpper();
+            }
+
+            // here either the bookID is valid or the bookID is -999
+            return bookID;
+        }
+
+        /// <summary>
+        ///  takes a string and returns true if it is a valid bookid
+        /// </summary>
+        /// <param name="bookID"></param>
+        /// <returns></returns>
+        static bool IsValidBookID(string bookID)
+        {
+            if (bookID.Length != 7 || !bookID.StartsWith('B'))
+                return false;
+
+            else
+            {
+                for (int i = 1; i < 7; i++)
+                {
+                    if (bookID[i] > '9' || bookID[i] < '0')
+                        return false;
+                }
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        ///  prompts the user to enter a score and checks if it is valid.
+        ///  It keeps prompting until it gets a valid score. In which case it returns it
+        /// </summary>
+        /// <returns>score</returns>
+
+        static double GetScoreFromUser()
+        {
+            Console.Write("Please enter a score >");
+            string scoreEntered = Console.ReadLine();
+            double score;
+            while (!(double.TryParse(scoreEntered, out score)) ||
+                score < 0 || score > 5)
+            {
+                Console.Write("Invalid score - please enter a score >");
+                scoreEntered = Console.ReadLine();
+            }
+            return score;
+        }
+
+
+        /// <summary>
+        /// Takes a double which is score between 1 and 5 (inclusive) and
+        /// returns a string with the category of that score.
+        /// </summary>
+        /// <param name="score"></param>
+        /// <returns></returns>
+        static string GetCategoryDescription(double score)
+        {
+            string ratingCategory;
+            switch (score)
+            {
+                case >= 0 and < 1:
+                    ratingCategory = "Poor";
+                    break;
+                case > 1 and <= 2:
+                    ratingCategory = "Fair";
+                    break;
+                case > 2 and <= 3:
+                    ratingCategory = "Good";
+                    break;
+                case > 3 and <= 4:
+                    ratingCategory = "Very Good";
+                    break;
+                case > 4 and <= 5:
+                    ratingCategory = "Excellant";
+                    break;
+                default:
+                    throw (new ArgumentOutOfRangeException($"Got {score} expecting an input between 1 and 5"));
+
+            }
+
+            return ratingCategory;
+        }
+    }
+}
---
+++ Una/Program.cs
@@ -0,0 +1,275 @@
+﻿/*
+
+ */
+using System.Net.Sockets;
+
+namespace CA1
+{
+    internal class Program
+    {
+        static int seats = 10;
+
+
+
+
+       
+
+        // Note: The three variables below are used to keep count
+        // of the seats, tickets and money
+        // by making them static they can be used and changed in any
+        // of the methods.
+
+
+        static int seatsOccupied;
+        static decimal totalMoneyTaken;
+        static int ticketsSold;
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            #region tests
+            //Q1 A
+            //Console.WriteLine("Q1 A Test Results");
+            //Console.WriteLine(CalculateTicketPrice(10, "Single") == 10); ;
+            //Console.WriteLine(CalculateTicketPrice(11, "Return") == 16.5m); ;
+            //Console.WriteLine(CalculateTicketPrice(-9, "Single") == -1); ;
+            //Console.WriteLine(CalculateTicketPrice(10, "") == 10); ;
+            //Console.WriteLine(CalculateTicketPrice(12, "return") == 18); ;
+
+            ////Q1 B
+            //Console.WriteLine("\nQ1 B Test Results");
+            //Console.WriteLine(ApplyDiscount(10, "Adult") == 10); ;
+            //Console.WriteLine(ApplyDiscount(11, "Child") == 5.5m); ;
+            //Console.WriteLine(ApplyDiscount(12, "Student") == 8.4m); ;
+            //Console.WriteLine(ApplyDiscount(10, "OAP") == 0); ;
+            //Console.WriteLine(ApplyDiscount(10, "oap") == 0); ;
+            //Console.WriteLine(ApplyDiscount(10, "travelcard") == 10); ;
+            //Console.WriteLine(ApplyDiscount(-11, "OAP") == -1); ;
+            //Console.WriteLine(ApplyDiscount(10, "") == 10); ;
+
+
+
+
+            GetCustomerTypeFromUser();
+            #endregion
+
+            //Q2 Ballin Bus
+            //Menu
+
+            Console.WriteLine("\nQ2 Ballin Bus\n");
+            int menuSelection = GetMenuChoice();
+
+
+ 
+            while (menuSelection != 4 )
+            {
+                
+                switch (menuSelection)                          //Switch statement for menu
+                {
+                    case 1:
+                        if (seatsOccupied >= seats)             //Check if its full before going into buying ticket
+                        {
+                            Console.WriteLine("Bus Full");
+                        }
+                        else
+                            BuyTicket();
+                        break;
+                    case 2:
+                        checkInReturnTicket();
+                        break;
+                    case 3:
+                        PrintJourneyReportToConsole();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("\nQ2 Ballin Bus\n");
+                menuSelection = GetMenuChoice();
+            }
+        }
+
+        /// <summary>
+        /// If the basePrice is negative this method returns -1.
+        /// If the ticket type is return this method returns the price of a return
+        /// ticket. Otherwise is returns the price of a single ticket.
+        /// </summary>
+        /// <param name="basePrice"></param>
+        /// <param name="ticketType"></param>
+        /// <returns>Price of a ticket or -1</returns>
+        static decimal CalculateTicketPrice(decimal basePrice, string ticketType)
+        {
+            decimal ticketCost = 0;
+            ticketType = ticketType.ToLower();
+            if (ticketCost < 0)
+            {
+                return -1;
+            }
+
+            switch (ticketType)
+            {
+                case "single":
+                    ticketCost = basePrice;
+                    break;
+                case "return":
+                    ticketCost = basePrice * 1.5m; // m for decimal (short for money)
+                    break;
+                default:
+                    ticketCost = basePrice;
+                    break;
+            }
+                return ticketCost;
+        }
+
+        /// <summary>
+        /// The method returns the price of a ticket after discount
+        /// based on the type of customer is applied.
+        /// </summary>
+        /// <param name="price"></param>
+        /// <param name="customerType"></param>
+        /// <returns>Dicount price </returns>
+        static decimal ApplyDiscount(decimal price, string customerType)
+        {
+            decimal discountedPrice = 0;
+            customerType = customerType.ToLower();
+
+            if (price < 0)
+            {
+                return -1;
+            }
+
+            switch (customerType)
+            {
+                case "child":
+                    discountedPrice = price * 0.5m;
+                    break;
+                case "student":
+                    discountedPrice = price * 0.7m;
+                    break;
+                case "oap":
+                    discountedPrice = 0;
+                    break;
+                default:
+                    discountedPrice = price;
+                    break;
+            }
+
+            return discountedPrice;
+
+        }
+
+        /// <summary>
+        /// This method prints out a menu and takes the user input
+        /// </summary>
+        /// <returns>The user input as an integer</returns>
+        static int GetMenuChoice()
+        {
+            int menuSelection;
+            Console.WriteLine("Ballin Buses Main Menu");
+            Console.WriteLine("\n1. Buy ticket");
+            Console.WriteLine("2. Check in return ticket");
+            Console.WriteLine("3. Journey Report");
+            Console.WriteLine("4. Exit.");
+            menuSelection = int.Parse(Console.ReadLine());
+            return menuSelection;
+        }
+
+        /// <summary>
+        /// This gets user input, it calcuates and outputs the price of a bus 
+        /// it also updates the static counter variables.
+        /// </summary>
+        static void BuyTicket()
+        {
+            Console.WriteLine("\n-=Buy Ticket=-");
+
+            decimal basePrice = 10;
+            string ticketType, customerType;
+            decimal ticketPrice;
+            //Input
+        
+            ticketType = GetTicketTypeFromUser();
+
+          
+            customerType = GetCustomerTypeFromUser();
+
+            // calculations
+
+            ticketPrice = CalculateTicketPrice(basePrice, ticketType);
+            ticketPrice = ApplyDiscount(ticketPrice, customerType);
+
+            // store the data
+
+            seatsOccupied++;
+            totalMoneyTaken += ticketPrice;
+            ticketsSold++;
+            
+
+            //Display ticket
+            Console.WriteLine($"Ballin-Sligo: {customerType} {ticketType} : {ticketPrice:c}");
+        }
+
+
+        static string GetCustomerTypeFromUser ()
+        {
+            Console.Write("Select type of customer (adult/child/student/oap): ");
+            string customerType = Console.ReadLine().ToLower();
+
+            string[] validTypes = { "adult", "child", "student", "oap" };
+
+            while (! validTypes.Contains(customerType))
+            {
+                Console.WriteLine("Incorrect Type, please choose again > ");
+                Console.Write("Select a ticket type (single, return): ");
+               customerType = Console.ReadLine().ToLower();
+            }
+
+            return customerType;
+            
+        }
+
+
+        static string GetTicketTypeFromUser()
+        {
+            Console.Write("Select a ticket type (single, return): ");
+            string ticketType = Console.ReadLine().ToLower();
+
+            while (ticketType != "single" && ticketType != "return")
+            {
+                Console.WriteLine("Incorrect Type, please choose again > ");
+                Console.Write("Select a ticket type (single, return): ");
+                ticketType = Console.ReadLine().ToLower();
+            }
+
+            return ticketType;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void checkInReturnTicket()
+        {
+            Console.WriteLine("\n-=Check In Return Ticket=-");
+            //Check if there is any seats remaining
+            if (seatsOccupied >= seats)
+            {
+                Console.WriteLine("Bus Full");
+            }
+            else
+            {
+                seatsOccupied = seatsOccupied + 1;
+                Console.WriteLine($"Number of seats remaining = {seats - seatsOccupied}");
+            }
+        }
+
+        static void PrintJourneyReportToConsole()
+        {
+            Console.WriteLine("\n-=Journey Report=-");
+            Console.WriteLine($"Number of Tickets Sold = {ticketsSold}");
+            Console.WriteLine($"Amount of Money Taken = {totalMoneyTaken:c}");
+            Console.WriteLine($"Total Seats Occupied = {seatsOccupied}");
+        }
+
+    }
+}
